8ed81c271dfd75a7de531750a97447fa0c511238
Артём Пашинин <a.pashinin@os-tech.ru>
2025-04-26 11:59:29 +0300

    fix

--- src/swipe/swipe.service.ts
+++ src/swipe/swipe.service.ts
@@ -127,7 +127,7 @@ export class SwipeService {
       if (swiperUser) {
         this.orderService.sendSwipeToOrder(
           ActionType.POS_RESPONSE,
-          updateSwipeDto.swiper_id,
+          updateSwipeDto.swiped_id,
           swiperUser.id
         );
       }
